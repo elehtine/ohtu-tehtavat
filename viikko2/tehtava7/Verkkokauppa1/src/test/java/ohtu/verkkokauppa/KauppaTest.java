9ae84630e9a552811ca4686f52c01f5a08598b80
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-11-21 17:40:41 +0200

    Exercise 4

--- viikko2/tehtava7/Verkkokauppa1/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko2/tehtava7/Verkkokauppa1/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -73,22 +73,12 @@ public class KauppaTest {
 
     @Test
     public void ostetaanKaksiSamaaTuotettaJoistaToinenLoppuJaPankinMetodiaTilisiirtoKutsutaan() {
-        // luodaan ensin mock-oliot
-        Pankki pankki = mock(Pankki.class);
-
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
-        // määritellään että viitegeneraattori palauttaa viitten 42
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
-        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10); 
         when(varasto.saldo(2)).thenReturn(0); 
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
         when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "voi", 7));
-
-        // sitten testattava kauppa 
-        Kauppa k = new Kauppa(varasto, pankki, viite);              
 
         // tehdään ostokset
         k.aloitaAsiointi();
@@ -98,5 +88,76 @@ public class KauppaTest {
 
         verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 5);
     }
+
+    @Test
+    public void asioinninAloittaminenNollaaVanhanOstoksen() {
+        when(viite.uusi()).thenReturn(42);
+
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("matti", "11111");
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 10);
+    }
+
+    @Test
+    public void pyytaaAinaUudenViitenumeron() {
+        when(viite.uusi())
+            .thenReturn(42)
+            .thenReturn(43)
+            .thenReturn(44);
+
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("matti", "11111");
+
+        verify(pankki).tilisiirto("matti", 42, "11111", "33333-44455", 5);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(1);
+        k.tilimaksu("teppo", "13579");
+
+        verify(pankki).tilisiirto("teppo", 43, "13579", "33333-44455", 15);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", 44, "12345", "33333-44455", 10);
+    }
+
+    @Test
+    public void oikeaSummaKunOstoskoristaOnPoistettuTuote() {
+        when(viite.uusi()).thenReturn(42);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(1);
+        k.poistaKorista(1);
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 10);
+
+    }
+
 
 }
